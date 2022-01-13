a3f6921e0eb8c3656adf4517d4a5573d6bbfe94d
MrSeaWave <MrDaemon@outlook.com>
2022-01-13 18:52:59 +0800

    chore: update

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -47,6 +47,9 @@ const config = {
             // targets: {
             //   node: 'current'
             // },
+            // 将modules: false否则 Babel 会在 Rollup 有机会做处理之前，
+            // 将我们的模块转成 CommonJS，导致 Rollup 的一些处理失败。
+            modules: false,
             loose: true
           }
         ]
