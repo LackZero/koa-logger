94fa0fba23e3537e3884c1e9d2ab1c22ae921ff3
MrSeaWave <MrDaemon@outlook.com>
2022-01-19 10:04:53 +0800

    chore: modify

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js'], // 指定要测试的文件类型的后缀
   testMatch: ['<rootDir>/**/*.test.js'],
-  testPathIgnorePatterns: ['/node_modules/']
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './coverage/',
+  collectCoverage: true
 };
